d012747d0841820887dbfb75c4bacc3406ade491
Lee Sarah <imsolem1226@gmail.com>
2020-07-23 00:34:30 +0900

    change models and add some data

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,26 @@
 from djongo import models
-
-# Create your models here.
+from django import forms
 
 class Party(models.Model):
     title = models.CharField(max_length=100)
+    '''
+    class Meta:
+        abstract = True
 
-
+class PartyForm(forms.ModelForm):
+    class Meta:
+        model = Party
+        fields = (
+            'title',
+        )
+'''
 class Player(models.Model):
     party = models.ForeignKey(Party, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     level = models.IntegerField()
+    '''
+    party_embed = models.EmbeddedField(
+        model_container=Party,
+        #model_form_class=PartyForm,
+    )
+'''
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Player, Party
 # Create your views here.
+
+def helloworld(request):
+    return HttpResponse('hello world!')
+
+def party_list(request):
+    return render (request, 'myapp/party_list.html',{})
