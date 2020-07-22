164f7138c6fea26e3f8a87d6bbca9f890819fa11
Lee Sarah <imsolem1226@gmail.com>
2020-07-22 23:04:44 +0900

    make models and create superuser

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+class Party(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Player(models.Model):
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    level = models.IntegerField()
