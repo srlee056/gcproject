53b301e8c5c19523e3989cdc086f9a3db614b617
Lee Sarah <imsolem1226@gmail.com>
2020-07-28 16:36:43 +0900

    implement modify plyers data and store them in db

--- parsed_data/urls.py
+++ parsed_data/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'guild'
 urlpatterns = [
-    path('', views.guild_player_list, name='player'),
+    path("", views.guild_player_list, name='player'),
+
+    path('<int:player_id>/', views.detail, name = 'detail'),
+
+    path('<int:player_id>/results/', views.results, name='results'),
+
+    path('<int:player_id>/vote/', views.vote, name='vote'),
 
 ]
--- parsed_data/views.py
+++ parsed_data/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import PlayerData
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def guild_player_list(request):
     
-    players = PlayerData.objects.order_by('-level')
+    players = PlayerData.objects.order_by('character')
     return render (request, 'parsed_data/guild_player_list.html',{'players':players})
+
+def detail(request, player_id):
+    player=get_object_or_404(PlayerData, pk=player_id)
+    return render(request, 'parsed_data/detail.html', {'player':player})
+
+def vote(request, player_id):
+    player=get_object_or_404(PlayerData, pk=player_id)
+    
+    
+    player.name = request.POST['p_name']
+    player.character = request.POST['p_character']
+    player.level = pk=request.POST['p_level']
+    player.save()
+
+    return HttpResponseRedirect(reverse('guild:results', args = (player.id,)))
+    
+def results(request, player_id):
+    player=get_object_or_404(PlayerData, pk=player_id)
+    return render(request, 'parsed_data/results.html', {'player':player})
